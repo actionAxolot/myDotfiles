ce874daf58871d50ea4df6347c773826beb3e874
mario <mario.r.vallejo@gmail.com>
2020-07-27 08:28:00 -0500

    Added ftpluging for typescript

--- home/.user_confs.zsh
+++ home/.user_confs.zsh
@@ -1,4 +1,4 @@
-source /usr/share/nvm/init-nvm.sh
+#source /usr/share/nvm/init-nvm.sh
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 
 HISTFILE=~/.zsh_history
@@ -14,4 +14,17 @@ alias free='free -m'                      # show sizes in MB
 alias np='nano -w PKGBUILD'
 alias more=less
 
-export VK_ICD_FILENAMES=/etc/vulkan/icd.d/nvidia_icd.json
+# export VK_ICD_FILENAMES=/etc/vulkan/icd.d/nvidia_icd.json
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# This is windows things
+if [ -z "$(pgrep ssh-agent)" ]; then
+        rm -rf /tmp/ssh-*
+        eval $(ssh-agent -s) > /dev/null
+      else
+        export SSH_AGENT_PID=$(pgrep ssh-agent)
+        export SSH_AUTH_SOCK=$(find /tmp/ssh-* -name agent.*)
+fi
+
